25100bd732a19604028c38e53eb203f0e1a39c6a
Zac Blazic <zacblazic@gmail.com>
2016-04-10 02:44:40 +0200

    Add message count notifications

--- app/channels/client/controllers/messages.js
+++ app/channels/client/controllers/messages.js
@@ -1,32 +1,49 @@
 'use strict';
 
 angular.module('app.channels')
-  .controller('MessagesCtrl', ['ChannelMessages', 'Socket', 'Users', 'channel', 'messages', 'user', function(ChannelMessages, Socket, Users, channel, messages, user) {
+  .controller('MessagesCtrl', ['$scope', 'ChannelMessages', 'Socket', 'Users', 'channel', 'messages', 'user', function($scope, ChannelMessages, Socket, Users, channel, messages, user) {
       var self = this;
       self.messages = messages;
       self.channelName = '# ' + channel.name;
       self.channel = channel;
+      self.channels = $scope.channelsCtrl.channels;
 
-      Socket.emit('joined channel', {
-        channelId: self.channel._id
-      });
+      // Remove listenters that we may be subscribed to previously
+      Socket.removeListener();
 
       Socket.on('message', function(message) {
         if (message.channel == self.channel._id) {
           self.messages.push(message);
+        } else {
+          for (var i = 0; i < self.channels.length; i++) {
+            var channel = self.channels[i];
+            if (channel._id == message.channel) {
+              channel.messageCount = channel.messageCount !== undefined ? channel.messageCount + 1 : 1;
+            }
+          }
         }
-
       });
+
+      self.getCurrentChannel = function() {
+        for (var i = 0; i < self.channels.length; i++) {
+          var channel = self.channels[i];
+
+          if (channel._id == self.channel._id) {
+            return channel;
+          }
+        }
+      };
+
+      var currentChannel = self.getCurrentChannel();
+      currentChannel.messageCount = null;
 
       self.message = '';
       self.sendMessage = function() {
-
         var message = {
           user: user._id,
           body: self.message,
           channel: channel._id
         };
-
 
         ChannelMessages.save({
           channelId: channel._id
@@ -36,15 +53,12 @@ angular.module('app.channels')
         }, function(data) {
 
           Users.get({ userId: message.user }, function(user) {
-
             message.user = user;
             message.created = Date.now();
 
             self.messages.push(message);
             Socket.emit('message', message);
-
           });
-
 
           self.message = '';
         });
--- app/channels/server/sockets/channels.js
+++ app/channels/server/sockets/channels.js
@@ -4,13 +4,12 @@ module.exports = function(io, socket) {
 
   console.log('user connected');
 
-  socket.on('joined channel', function(channel) {
-    console.log(channel);
-    socket.join(channel.channelId);
+  socket.on('message', function(message) {
+    socket.broadcast.emit('message', message);
   });
 
-  socket.on('message', function(message) {
-    socket.broadcast.to(message.channel).emit('message', message);
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
   });
 
 };
